e88186a5d8828d227434c2280a2498e9f51a7e0d
TobyWans <62607977+TobyWans@users.noreply.github.com>
2023-03-17 15:17:10 +0000

    Bresenham Circle algo

--- src/renderer/gridOperations.cpp
+++ src/renderer/gridOperations.cpp
@@ -8,19 +8,10 @@ void Renderer::reset_grid() {
     }
 }
 
-void Renderer::fill_grid(int x, int y, int dx, int dy, Material state) {
-
-    int fx = abs(dx - x);
-    int fy = abs(dy - y);
-    int sx = x < dx ? 1 : -1;
-    int sy = y < dy ? 1 : -1;
-    int err = fx - fy;
-
+void Renderer::draw_circle(int xc, int yc, int x, int y, Material state) {
     int brush_rad = brush_size / 2;
-
-    while (true) {
-        for (int xi = x - brush_rad; xi < x + brush_rad; xi++) {
-            for (int yi = y - brush_rad; yi < y + brush_rad; yi++) {
+    for (int xi = xc - brush_rad; xi < xc + brush_rad; xi++) {
+            for (int yi = yc - brush_rad; yi < yc + brush_rad; yi++) {
                 if (xi < SCREEN_WIDTH && yi < SCREEN_HEIGHT - (2 * SCREEN_PADDING) && xi >= 0 && yi >= 0) {
                     if (grid[xi / GRID_CELL_SIZE][yi / GRID_CELL_SIZE].material == NONE || state == NONE || state == WALL) {
                         grid[xi / GRID_CELL_SIZE][yi / GRID_CELL_SIZE].material = state;
@@ -30,17 +21,51 @@ void Renderer::fill_grid(int x, int y, int dx, int dy, Material state) {
                 }
             }
         }
-        if (x == dx && y == dy) {
-            break;
+}
+
+void Renderer::bresenham_algo(int xc, int yc, int r, Material state) {
+    int x = 0, y = r;
+    int d = 3 - 2 * r;
+    draw_circle(xc, yc, x, y, state);
+    while (x <= y) {
+        x++;
+        if (d > 0) {
+            y--;
+            d = d + 4 * (x - y) + 10;
         }
-        int e2 = 2 * err;
-        if (e2 > -fy) {
-            err -= fy;
-            x += sx;
+        else {
+            d = d + 4 * x + 6;
         }
-        if (e2 < fx) {
-            err += fx;
-            y += sy;
+        draw_circle(xc, yc, x, y, state);
+    }
+}
+
+void Renderer::fill_grid(int xa, int ya, int xb, int yb, Material state) {
+    int dx = abs(xa - xb), dy = abs(ya - yb);
+    int p = 2 * dy - dx;
+    int twoDy = 2 * dy, twoDyMinusDx = 2 * (dy - dx);
+    int x, y, xEnd;
+    int brush_rad = brush_size / 2;
+    if (xa > xb) {
+        x = xb;
+        y = yb;
+        xEnd = xa;
+    }
+    else {
+        x = xa;
+        y = ya;
+        xEnd = xb;
+    }
+    bresenham_algo(x, y, brush_rad, state);
+    while (x < xEnd) {
+        x++;
+        if (p < 0) {
+            p += twoDy;
         }
+        else {
+            y++;
+            p += twoDyMinusDx;
+        }
+        bresenham_algo(x, y, brush_rad, state);
     }
 }
--- src/renderer/renderer.h
+++ src/renderer/renderer.h
@@ -15,12 +15,14 @@ class Renderer {
         SDL_Renderer *renderer;
         SDL_Window *window;
         int brush_size = 5; // must be odd
-        const char *header;
+        const char *mat_text;
         Pixle grid[GRID_WIDTH][GRID_HEIGHT] = { NONE };
         SDL_Color background_color = {242,240,229};
         bool scan_direction = true;
     protected:
         void render();
+
+        void UI_layer();
 
         void draw_text(const char *text, int posx, int posy, int size, SDL_Color color);
         void draw_text(const char *text, int posx, int posy, int size);
@@ -30,6 +32,8 @@ class Renderer {
         void draw_text(const int text, int posx, int posy);
         
         void reset_grid();
+        void draw_circle(int, int, int, int, Material);
+        void bresenham_algo(int, int, int, Material);
         void fill_grid(int, int, int ,int , Material);
 
         void update();
